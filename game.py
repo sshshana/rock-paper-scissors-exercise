bf9d2b7c8a5c220a5eb1cfc3062a73a3242dd67b
Shana Shin <sshshana@gmail.com>
2021-02-04 07:40:10 +0900

    user input + validation

--- game.py
+++ game.py
@@ -1,3 +1,51 @@
 # game.py
 
 print("Rock, Paper, Scissors, Shoot!")
+
+
+
+print("-------------------")
+print("Welcome 'Player One' to my Rock-Paper-Scissors game...")
+print("-------------------")
+
+#asking user for an input
+
+x = input("Please choose either 'rock', 'paper', or 'scissors':")
+
+if x == "rock" or x == "paper" or x == "scissors":
+    print("You chose:", x)
+
+else:
+    print("You entered:", x, ". Please enter a valid input!")
+
+
+
+
+
+
+
+exit()
+
+
+
+
+
+
+
+
+
+
+
+# simulating a computer input
+
+print("The computer chose: 'paper'")
+
+
+
+
+# determining who won
+
+print("-------------------")
+print("Oh, the computer won. It's ok.")
+print("-------------------")
+print("Thanks for playing. Please play again!")

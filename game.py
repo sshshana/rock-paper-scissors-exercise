d9c080a4b93ac1ecd56120714a05cfbb3ce57afd
Shana Shin <sshshana@gmail.com>
2021-02-04 07:52:41 +0900

    simulate computer input

--- game.py
+++ game.py
@@ -10,40 +10,35 @@ print("-------------------")
 
 #asking user for an input
 
-x = input("Please choose either 'rock', 'paper', or 'scissors':")
+user_choice = input("Please choose either 'rock', 'paper', or 'scissors': ")
 
-if x == "rock" or x == "paper" or x == "scissors":
-    print("You chose:", x)
+if user_choice == "rock" or user_choice == "paper" or user_choice == "scissors":
+    print("You chose:", user_choice)
 
 else:
-    print("You entered:", x, ". Please enter a valid input!")
-
-
-
-
-
-
-
-exit()
-
-
-
-
-
-
-
-
-
+    print("You entered:", user_choice, ". Please enter a valid input!")
+    exit()
 
 
 # simulating a computer input
 
-print("The computer chose: 'paper'")
+import random
 
+arr = ["rock", "paper", "scissors"]
 
+random.shuffle(arr)
+arr
 
+computer_choice = random.choice(arr)
+
+print(f"The computer chose: {computer_choice}")
 
 # determining who won
+
+if user_choice == "rock":
+    if computer_choice == "rock":
+        print("You ")
+
 
 print("-------------------")
 print("Oh, the computer won. It's ok.")
